bf606d3f661eb5637dcc79182ee5b938b770083e
Thibaut Girier <30323597+tgirier@users.noreply.github.com>
2020-09-04 11:40:28 +0200

    Add test multiply (#4.1)

--- calculator_test.go
+++ calculator_test.go
@@ -22,3 +22,12 @@ func TestSubtract(t *testing.T) {
 		t.Errorf("want %f, got %f", want, got)
 	}
 }
+
+func TestMultiply(t *testing.T) {
+	t.Parallel()
+	var want float64 = 6
+	got := calculator.Multiply(3, 2)
+	if want != got {
+		t.Errorf("want %f, got %f", want, got)
+	}
+}
