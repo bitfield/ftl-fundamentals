e158e7ef26e27524a1ea4b611ac7f7e9c711cc3d
Shivdas Patil <shivdas.patil@deskera.com>
2020-09-06 00:51:58 +0530

    Add solutions for GOAL exercises 1 to 6

--- calculator.go
+++ calculator.go
@@ -1,13 +1,32 @@
 // Package calculator provides a library for simple calculations in Go.
 package calculator
 
+import (
+	"errors"
+)
+
 // Add takes two numbers and returns the result of adding them together.
 func Add(a, b float64) float64 {
-return a + b
+	return a + b
 }
 
 // Subtract takes two numbers and returns the result of subtracting the second
 // from the first.
 func Subtract(a, b float64) float64 {
-	return b - a
+	return a - b
+}
+
+// Multiply takes two numbers and returns the result of multiplication of both
+// the numbers
+func Multiply(a, b float64) float64 {
+	return a * b
+}
+
+// Divide takes two numbers and returns the result of dividing the first number
+// by second number. It returns error if the division operation is not valid
+func Divide(a, b float64) (float64, error) {
+	if b == 0 {
+		return 0, errors.New("Divide by Zero")
+	}
+	return a / b, nil
 }
--- calculator_test.go
+++ calculator_test.go
@@ -2,6 +2,7 @@ package calculator_test
 
 import (
 	"calculator"
+	"math"
 	"testing"
 )
 
@@ -14,11 +15,182 @@ func TestAdd(t *testing.T) {
 	}
 }
 
-// func TestSubtract(t *testing.T) {
-// 	t.Parallel()
-// 	var want float64 = 2
-// 	got := calculator.Subtract(4, 2)
-// 	if want != got {
-// 		t.Errorf("want %f, got %f", want, got)
-// 	}
-// }
+
+func almostEqual(a, b float64) bool {
+	const float64EqualityThreshold = 1e-9
+	return math.Abs(a-b) <= float64EqualityThreshold
+}
+
+func TestSubtract(t *testing.T) {
+	t.Parallel()
+	var successTestCases = []struct {
+		name string
+		want float64
+		a    float64
+		b    float64
+	}{
+		{
+			name: "Two positive numbers",
+			want: 3,
+			a:    5,
+			b:    2,
+		},
+		{
+			name: "Zero subtracted with any number",
+			want: -2,
+			a:    0,
+			b:    2,
+		},
+		{
+			name: "Positive and negative number",
+			want: -9,
+			a:    -5,
+			b:    4,
+		},
+		{
+			name: "Two negative numbers",
+			want: -5,
+			a:    -8,
+			b:    -3,
+		},
+		{
+			name: "Two fractional numbers",
+			want: 0.1,
+			a:    3.3,
+			b:    3.2,
+		},
+	}
+
+	for _, test := range successTestCases {
+		got := calculator.Subtract(test.a, test.b)
+		if !almostEqual(test.want, got) {
+			t.Errorf("Test Subtract(%f, %f) %s failed: want %f, got %f",
+				test.a, test.b, test.name, test.want, got)
+		}
+	}
+}
+
+func TestMultiply(t *testing.T) {
+	t.Parallel()
+	var successTestCases = []struct {
+		name string
+		want float64
+		a    float64
+		b    float64
+	}{
+		{
+			name: "Two positive numbers",
+			want: 10,
+			a:    5,
+			b:    2,
+		},
+		{
+			name: "Zero multiplied with any number",
+			want: 0,
+			a:    0,
+			b:    2,
+		},
+		{
+			name: "Positive and negative number",
+			want: -20,
+			a:    -5,
+			b:    4,
+		},
+		{
+			name: "Two negative numbers",
+			want: 24,
+			a:    -8,
+			b:    -3,
+		},
+		{
+			name: "Two fractional numbers",
+			want: 4.03,
+			a:    1.3,
+			b:    3.1,
+		},
+	}
+
+	for _, test := range successTestCases {
+		got := calculator.Multiply(test.a, test.b)
+		if !almostEqual(test.want, got) {
+			t.Errorf("Test Multiple(%f, %f) %s failed: want %f, got %f",
+				test.a, test.b, test.name, test.want, got)
+		}
+	}
+
+}
+
+func TestDivide(t *testing.T) {
+	t.Parallel()
+	var successTestCases = []struct {
+		name        string
+		want        float64
+		a           float64
+		b           float64
+		errExpected bool
+	}{
+		{
+			name:        "Two positive numbers",
+			want:        5,
+			a:           10,
+			b:           2,
+			errExpected: false,
+		},
+		{
+			name:        "Zero divided with any number",
+			want:        0,
+			a:           0,
+			b:           2,
+			errExpected: false,
+		},
+		{
+			name:        "Positive and negative number",
+			want:        -1.25,
+			a:           -5,
+			b:           4,
+			errExpected: false,
+		},
+		{
+			name:        "Two negative numbers",
+			want:        4,
+			a:           -8,
+			b:           -2,
+			errExpected: false,
+		},
+		{
+			name:        "Two fractional numbers",
+			want:        2.75,
+			a:           3.3,
+			b:           1.2,
+			errExpected: false,
+		},
+		{
+			name:        "Divide by zero",
+			want:        0,
+			a:           2,
+			b:           0,
+			errExpected: true,
+		},
+	}
+
+	for _, test := range successTestCases {
+		got, err := calculator.Divide(test.a, test.b)
+		if test.errExpected {
+			if err == nil {
+				t.Errorf("Test Divide (%f, %f) %s failed: Expected error got nil",
+					test.a, test.b, test.name)
+			}
+		} else {
+			if err != nil {
+				t.Errorf("Test Divide (%f, %f) %s failed: Got err expected nil",
+					test.a, test.b, test.name)
+			}
+			if !almostEqual(test.want, got) {
+				t.Errorf("Test Divide (%f, %f) %s failed: want %f, got %f",
+					test.a, test.b, test.name, test.want, got)
+			}
+		}
+
+	}
+
+}
