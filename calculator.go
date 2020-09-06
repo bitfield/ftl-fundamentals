9c3027fe4e8479513cc75eccbac15fcda06cd56d
Thibaut Girier <30323597+tgirier@users.noreply.github.com>
2020-09-06 15:50:58 +0200

    Add divide logic (#6.4)

--- calculator.go
+++ calculator.go
@@ -1,6 +1,8 @@
 // Package calculator provides a library for simple calculations in Go.
 package calculator
 
+import "errors"
+
 // Add takes two numbers and returns the result of adding them together.
 func Add(a, b float64) float64 {
 	return a + b
@@ -15,4 +17,13 @@ func Subtract(a, b float64) float64 {
 // Multiply takes two numbers and returns the result of multiplying them together.
 func Multiply(a, b float64) float64 {
 	return a * b
+}
+
+// Divide takes two number and returns the result of dividing the first
+// with the second. If the second number is zero it returns an error.
+func Divide(a, b float64) (float64, error) {
+	if b == 0 {
+		return 0, errors.New("division by 0")
+	}
+	return a / b, nil
 }
