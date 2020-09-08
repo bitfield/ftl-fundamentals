dffc17033cd0307aeef18044225dc5614b024b15
Thomas Kling <thomas.kling@banno.com>
2020-09-08 14:08:26 -0500

    Adding test cases for TestAdd and iterating through them in the test

--- calculator_test.go
+++ calculator_test.go
@@ -13,11 +13,29 @@ type testCase struct {
 }
 
 func TestAdd(t *testing.T) {
+	cases := []testCase{
+		{"Two positive numbers", 2, 2, 4},
+		{"Two negative numbers", -2, -2, -4},
+		{"One negative and one positive number equaling zero", -1, 1, 0},
+		{"One fractional and one whole number", 5.4, 2, 7.4},
+		{"Two fractional numbers", 2.3, 4.3, 6.6},
+		{"Two fractional numbers equaling a whole number", 2.3, 3.7, 6},
+		{"Two negative fractional numbers", -1.5, -2.5, -4},
+	}
+
 	t.Parallel()
-	var want float64 = 4
-	got := calculator.Add(2, 2)
-	if want != got {
-		t.Errorf("want %f, got %f", want, got)
+	for _, c := range cases {
+		t.Run(c.name, testAddFunc(c))
+	}
+}
+
+func testAddFunc(c testCase) func(*testing.T) {
+	return func(t *testing.T) {
+		var want float64 = c.expected
+		got := calculator.Add(c.firstInput, c.secondInput)
+		if want != got {
+			t.Errorf("want %f, got %f", want, got)
+		}
 	}
 }
 
