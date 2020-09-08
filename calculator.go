281e8f84dda33111978537c0206320dbc35e9ad4
Thomas Kling <thomas.kling@banno.com>
2020-09-08 13:38:20 -0500

    Uncomment Subtract test.  Fix Substract function so that it matches the comment and passes the test.

--- calculator.go
+++ calculator.go
@@ -9,5 +9,5 @@ func Add(a, b float64) float64 {
 // Subtract takes two numbers and returns the result of subtracting the second
 // from the first.
 func Subtract(a, b float64) float64 {
-	return b - a
+	return a - b
 }
