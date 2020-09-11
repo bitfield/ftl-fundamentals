e35062494cbe7792eb2ece8dc50962aade049290
Thomas Kling <thomas.kling@banno.com>
2020-09-11 09:39:20 -0500

    Actually test a negative number with Sqrt

--- calculator_test.go
+++ calculator_test.go
@@ -135,7 +135,7 @@ func TestAddRandom(t *testing.T) {
 func TestSqrt(t *testing.T) {
 	cases := []testCaseWithErr{
 		{"Square root of zero should be zero", 0, 0, false, 0},
-		{"Square root of a negative number should return an error", 0, 0, true, 0},
+		{"Square root of a negative number should return an error", -4, 0, true, 0},
 		{"Square root of a positive whole number", 4, 0, false, 2},
 	}
 
