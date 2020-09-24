3aec813bff44f880f03b45d54240525ccf627fbb
Abhjit L <abhijit.laghate@gmail.com>
2020-09-24 23:40:35 +0530

    Continuing the journey of TDD

--- calculator.go
+++ calculator.go
@@ -1,13 +1,30 @@
 // Package calculator provides a library for simple calculations in Go.
 package calculator
 
+import "errors"
+
 // Add takes two numbers and returns the result of adding them together.
 func Add(a, b float64) float64 {
-return a + b
+	return a + b
 }
 
 // Subtract takes two numbers and returns the result of subtracting the second
 // from the first.
 func Subtract(a, b float64) float64 {
 	return b - a
+}
+
+// Multiply takes two numbers and returns the result of multplying the second
+// by the first.
+func Multiply(a, b float64) float64 {
+	return b * a
+}
+
+// Divide takes two numbers and returns the result of dividing the second
+// by the first.
+func Divide(a, b float64) (float64, error) {
+	if a != 0 {
+		return b / a, nil
+	}
+	return 0, errors.New("math: Divide by Zero")
 }
--- calculator_test.go
+++ calculator_test.go
@@ -5,13 +5,26 @@ import (
 	"testing"
 )
 
+type testCase struct {
+	a      float64
+	b      float64
+	want   float64
+	tcName string
+}
+
 func TestAdd(t *testing.T) {
 	t.Parallel()
-	var want float64 = 4
-	got := calculator.Add(2, 2)
-	if want != got {
-		t.Errorf("want %f, got %f", want, got)
+	tCases := []testCase{
+		{2, 2.030000, 4.030000, "some fractions1"},
+		{6, 3.99, 9.99, "some fractions2"},
+		{2, 2.03675, 4.03675, "some fractions3"},
 	}
+	for _, tc := range tCases {
+		if got := calculator.Add(tc.a, tc.b); tc.want != got {
+			t.Errorf("want %f, got %f :: Test: %s", tc.want, got, tc.tcName)
+		}
+	}
+
 }
 
 func TestSubtract(t *testing.T) {
@@ -20,5 +33,28 @@ func TestSubtract(t *testing.T) {
 	got := calculator.Subtract(2, 4)
 	if want != got {
 		t.Errorf("want %f, got %f", want, got)
+	}
+}
+
+func TestMultiply(t *testing.T) {
+	t.Parallel()
+	var want float64 = 8
+	got := calculator.Multiply(2, 4)
+	if want != got {
+		t.Errorf("want %f, got %f", want, got)
+	}
+}
+
+func TestDivide(t *testing.T) {
+	t.Parallel()
+	tCases := []testCase{
+		{2.030000, 20, 9.852216, "some fractions1"},
+		{0, 3.99, 0, "some fractions2"},
+		{2, 0, 0, "some fractions3"},
+	}
+	for _, tc := range tCases {
+		if got := calculator.Add(tc.a, tc.b); tc.want != got {
+			t.Errorf("want %f, got %f :: Test: %s", tc.want, got, tc.tcName)
+		}
 	}
 }
